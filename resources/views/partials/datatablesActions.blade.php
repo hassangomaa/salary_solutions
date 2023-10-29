1578f8cc52b19c026976a8e86f60fab6fcb44fd9
hassangomaa <hassangomaa974@gmail.com>
2023-10-29 17:08:47 +0200

    show delete permnaly only for route employee index

--- resources/views/partials/datatablesActions.blade.php
+++ resources/views/partials/datatablesActions.blade.php
@@ -23,12 +23,13 @@
 {{--</a>--}}
 
 
-<form action="{{  route('employee.deletePermanent', $row->id) }}" method="POST"
-{{--      onsubmit="return confirm('{{ trans('global.areYouSure') }}');"--}}
-      style="display: inline-block;">
+@if (Str::startsWith(Route::currentRouteName(), 'employee.'))
+    <form action="{{ route('employee.deletePermanent', $row->id) }}" method="POST"
+          onsubmit="return confirm('{{ trans('global.areYouSure') }}');"
+          style="display: inline-block;">
 
-    <input type="hidden" name="_method" value="DELETE">
-    <input type="hidden" name="_token" value="{{ csrf_token() }}">
-    <input type="submit" class="btn btn-xs btn-danger" value="   {{ trans('global.permanent_delete') }}  ">
-</form>
-
+        <input type="hidden" name="_method" value="DELETE">
+        <input type="hidden" name="_token" value="{{ csrf_token() }}">
+        <input type="submit" class="btn btn-xs btn-danger" value="{{ trans('global.permanent_delete') }}">
+    </form>
+@endif
